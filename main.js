a79a9730164b7c77e56729ff61a2ebef3145610b
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-06-29 15:49:34 -0400

    Added new animation class to winner element, setTimeout for the removal 3.5s

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ const renderGameboard = () => {
     }
     cell.textContent = currentMarker;
 
-
     // Add a click event listener to the cell
     cell.addEventListener("click", () => {
       // Call the playRound function and pass the cell index
@@ -65,15 +64,28 @@ const playRound = (index) => {
   // Update the cell on the game board with the current player's marker
   const marker = currentPlayer.getMarker();
   const isCellUpdated = gameboard.updateCell(index, marker);
-  const scoreBoard = document.querySelector('.stats__container-score')
+  const scoreBoard = document.querySelector(".stats__container-score");
 
   if (isCellUpdated) {
     // Check if the current player has won
     const isWinner = checkWin(marker, currentPlayer);
     if (isWinner) {
-        // Announce the winner
-        let winnerElement = document.querySelector('.winner');
-      winnerElement.textContent = ` ${currentPlayer.getName()} is the winner!`
+      // Announce the winner
+      let winnerElement = document.querySelector(".winner");
+      winnerElement.textContent = ` ${currentPlayer.getName()} is the winner!`;
+      // Add the "show" class to trigger the fade animation
+      winnerElement.classList.add("animate");
+
+      // Set a timeout to remove the "show" class and reset the opacity after the animation
+      setTimeout(() => {
+        winnerElement.classList.remove("animate");
+      }, 3500);
+
+      // // Reveal the winning crown image for the current player
+      // const currentPlayerIndex = currentPlayer === player1 ? 1 : 2;
+      // const crownImage = document.getElementById(`crown${currentPlayerIndex}`);
+      // crownImage.classList.remove("hidden");
+
       isGameOver = true;
     }
 
@@ -87,52 +99,55 @@ const playRound = (index) => {
   }
 };
 const checkWin = (marker) => {
-    // Define the winning combinations
-    const winningCombos = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-      [0, 4, 8], [2, 4, 6] // Diagonals
-    ];
+  // Define the winning combinations
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
 
-    // Iterate through each winning combination
-    for (const combo of winningCombos) {
-      let isWinningCombo = true; // Flag to track if all cells in the combination match the marker
+  // Iterate through each winning combination
+  for (const combo of winningCombos) {
+    let isWinningCombo = true; // Flag to track if all cells in the combination match the marker
 
-      // Check each index in the current combination
-      for (const index of combo) {
-        const cellMarker = gameboard.getBoard()[index]; // Get the marker at the current index
+    // Check each index in the current combination
+    for (const index of combo) {
+      const cellMarker = gameboard.getBoard()[index]; // Get the marker at the current index
 
-        // Check if the cell marker matches the player's marker and is not empty
-        if (cellMarker !== marker || cellMarker === '') {
-          isWinningCombo = false; // Set the flag to false if the marker doesn't match or is empty
-          break; // Exit the loop, no need to check further
-        }
-      }
-
-      // If all cells in the combination match the marker, return true (win)
-      if (isWinningCombo) {
-        // Increment the winning player's score
-        currentPlayer.incrementScore();
-        // Play a winning sound
-        const audioElement = new Audio("./winner.mp3");
-        audioElement.play();
-
-
-        // Update the scoreboard elements
-        const player1ScoreElement = document.querySelector('.player1-score');
-        const player2ScoreElement = document.querySelector('.player2-score');
-
-        player1ScoreElement.textContent = player1.getScore(); // Update player 1's score
-        player2ScoreElement.textContent = player2.getScore(); // Update player 2's score
-
-        return true;
+      // Check if the cell marker matches the player's marker and is not empty
+      if (cellMarker !== marker || cellMarker === "") {
+        isWinningCombo = false; // Set the flag to false if the marker doesn't match or is empty
+        break; // Exit the loop, no need to check further
       }
     }
 
-    // No winning combination found, return false (no win)
-    return false;
-  };
+    // If all cells in the combination match the marker, return true (win)
+    if (isWinningCombo) {
+      // Increment the winning player's score
+      currentPlayer.incrementScore();
+      // Play a winning sound
+      const audioElement = new Audio("./winner.mp3");
+      audioElement.play();
 
+      // Update the scoreboard elements
+      const player1ScoreElement = document.querySelector(".player1-score");
+      const player2ScoreElement = document.querySelector(".player2-score");
+
+      player1ScoreElement.textContent = player1.getScore(); // Update player 1's score
+      player2ScoreElement.textContent = player2.getScore(); // Update player 2's score
+
+      return true;
+    }
+  }
+
+  // No winning combination found, return false (no win)
+  return false;
+};
 
 // Restarts the game
 const restartButton = document.querySelector("#restartButton");
